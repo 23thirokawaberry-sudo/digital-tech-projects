c9478ba0b18ba429e0e1c08b1d09d048d7420aa2
23thirokawaberry-sudo <23thirokawaberry@wakatipu.school.nz>
2026-03-13 18:42:09 +1300

    3/13/2026

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,6 @@
-print("Hello world")
-print("Coffee")
+dealership = {
+    "car1": {"company": "Toyota", "model": "Carola", "date": "2010"},
+    "car2": {"company": "Tesla", "model": "Cybertruck", "date": "2024"},
+    "car3": {"model": "SUPERCAR10", "date": "1997 SO RETRO"}
+}
+
