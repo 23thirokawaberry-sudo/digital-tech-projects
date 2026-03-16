0e57c736195cf65acebbbdf95e0df3af016b1ec6
23thirokawaberry-sudo <23thirokawaberry@wakatipu.school.nz>
2026-03-16 14:47:10 +1300

    3/16/26

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,12 @@
-dealership = {
-    "car1": {"company": "Toyota", "model": "Carola", "date": "2010"},
-    "car2": {"company": "Tesla", "model": "Cybertruck", "date": "2024"},
-    "car3": {"model": "SUPERCAR10", "date": "1997 SO RETRO"}
+students = {
+    "1": {
+        "name": "Greg",
+        "age": 16,
+        "subjects": ["English", "Math", "Science", "P.E.", "Art"]
+    },
+    "2": {
+        "name": "Sam",
+        "age": 15,
+        "subjects": ["English", "Math", "Digital tech", "Music", "Art"]
+    }
 }
-
